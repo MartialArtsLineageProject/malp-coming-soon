a1aff2b0f52632438d28650887944638be6bb127
Marcus Moore <marcusmooreweb@gmail.com>
2022-02-21 17:18:45 -0800

    adjustment to app

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <HeroCarousel />
+      
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>Coming soon...</p>
