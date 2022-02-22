0708da699dc7ca69ac62dadceee1ab3aa60476f8
Marcus Moore <marcusmooreweb@gmail.com>
2022-02-21 16:27:36 -0800

    added react-hero-carousel and switched some logo files

---
+++ src/hero-carousel.js
@@ -0,0 +1,22 @@
+import React from "react";
+import HeroCarousel from "react-hero-carousel";
+
+export default () => (
+  <HeroCarousel>
+    <img
+      src="https://placem.at/places?w=1920&h=1080&seed=1&txt=1"
+      width="100%"
+      height="100%"
+    />
+    <img
+      src="https://placem.at/places?w=1920&h=1080&seed=2&txt=2"
+      width="100%"
+      height="100%"
+    />
+    <img
+      src="https://placem.at/places?w=1920&h=1080&seed=2&txt=2"
+      width="100%"
+      height="100%"
+    />
+  </HeroCarousel>
+);
